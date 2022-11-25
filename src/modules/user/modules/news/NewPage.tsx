32b91f26c7f5f891f90d50cefbe69bea4bc92e4c
vungocduyet24 <vungocduyet24@gmail.com>
2022-11-25 23:46:38 +0700

    show category

--- src/modules/user/modules/news/NewPage.tsx
+++ src/modules/user/modules/news/NewPage.tsx
@@ -2,25 +2,38 @@ import LayoutFullUser from '../../component/layout';
 import image1 from '../../../../atoms/images/312221646_771694890930297_6215795452150606467_n.png';
 import image2 from '../../../../atoms/images/309911559_766777848088668_7456215299439506121_n.png';
 import image3 from '../../../../atoms/images/292355658_1935330136857061_8603834351602225961_n.png';
+import { categoryApis } from 'src/apis/admin';
+import { useEffect, useState } from 'react';
 
 const NewPage = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const { data } = await categoryApis.findAll();
+      setCategories(data.data);
+    })();
+  }, []);
+
   return (
     <LayoutFullUser>
       <div className="flex">
         <div className="w-[calc(20%-100px)] sticky top-[10px] ml-[50px] mr-[20px] my-[20px] bg-white shadow-sm rounded-sm p-4 h-[500px]">
           <h3 className="text-xl font-semibold text-gray-700 mb-3 font-roboto">Categories</h3>
           <div className="space-y-2">
-            <a
-              href="#"
-              className="flex leading-4 items-center text-gray-700 font-semibold text-sm uppercase transition hover:text-blue-500"
-            >
-              <span className="mr-2">
-                <i className="far fa-folder-open"></i>
-              </span>
-              <span>Beauti</span>
-              <p className="ml-auto font-normal">(12)</p>
-            </a>
-            <a
+            {categories.map((item: any) => (
+              <a
+                href="#"
+                className="flex leading-4 items-center text-gray-700 font-semibold text-sm uppercase transition hover:text-blue-500"
+              >
+                <span className="mr-2">
+                  <i className="far fa-folder-open"></i>
+                </span>
+                <span>{item.name}</span>
+                <p className="ml-auto font-normal">{item.posts.length}</p>
+              </a>
+            ))}
+            {/* <a
               href="#"
               className="flex leading-4 items-center text-gray-700 font-semibold text-sm uppercase transition hover:text-blue-500"
             >
@@ -109,7 +122,7 @@ const NewPage = () => {
               </span>
               <span>Technology</span>
               <p className="ml-auto font-normal">(17)</p>
-            </a>
+            </a> */}
           </div>
         </div>
         <div className="my-[20px] ">
